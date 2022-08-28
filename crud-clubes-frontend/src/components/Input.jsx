7855e77d9ea6803d625d5974398ee9852d21a0d4
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-08-27 23:06:28 -0300

    Create Input.jsx and modify EditModal to use it

---
+++ crud-clubes-frontend/src/components/Input.jsx
@@ -0,0 +1,10 @@
+function Input({name, type, placeholder}) {
+  return (
+    <div className="relative">
+      <input type={type} name={name} id={name} className="block px-2.5 pb-2.5 pt-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white peer" placeholder=" " />
+      <label htmlFor={name} className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">{placeholder}</label>
+    </div>
+  );
+}
+
+export default Input
