86d5392104b7b9e7a6f621157e79a9846315c667
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-08-27 22:53:22 -0300

    Update Menu.jsx

--- crud-clubes-frontend/src/components/Menu.jsx
+++ crud-clubes-frontend/src/components/Menu.jsx
@@ -1,11 +1,22 @@
-import Item from "./Item";
+import Item from './Item';
 
-function Menu({teamList}) {
+function Menu({teams, setFocusedTeam, showDeleteModal, showEditModal}) {
   return (
-    <div>
-      {teamList.map(({imgCrest,tla}) => (
-        <Item key={tla} tla={tla} img={imgCrest}/>
-      ) )}
+    <div className=''>
+      <table className=''>
+        <thead>
+    <tr className=''>
+      <th className=''>Crest</th>
+      <th className=''>Name</th>
+      <th className=''>Actions</th>
+    </tr>
+    </thead>
+      {teams.map(({crestUrl,tla,name}) => (
+        <Item key={tla} tla={tla} name={name} crestUrl={crestUrl} setFocusedTeam={setFocusedTeam} showDeleteModal={showDeleteModal} showEditModal={showEditModal}/>
+      ))}
+      </table>
     </div>
   )
 }
+
+export default Menu
