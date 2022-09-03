2f0d1c16e4eec88c23cb3419ed7de0340f6f5c6b
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-09-03 00:48:06 -0300

    Update Item.jsx with Button component

--- crud-clubes-frontend/src/components/Item.jsx
+++ crud-clubes-frontend/src/components/Item.jsx
@@ -1,3 +1,5 @@
+import Button from "./Button";
+
 function Item({crestUrl, tla, name, fetchTeam, showDeleteModal, showEditModal}) {
   return (
     <tbody>
@@ -11,9 +13,9 @@ function Item({crestUrl, tla, name, fetchTeam, showDeleteModal, showEditModal}) 
           </td>
           <td className='py-4 px-6 border-b border-grey-light'>{name}</td>
           <td className='py-4 px-6 border-b border-grey-light'>
-            <a href='#' className='text-white  font-bold py-1 px-3 mx-1 rounded text-xs bg-blue-600 hover:bg-blue-800 hover:text-white'>View</a>
-            <a href='#' className='text-white  font-bold py-1 px-3 mx-1 rounded text-xs bg-green-600 hover:bg-green-800 hover:text-white' onClick={() => {showEditModal(); fetchTeam(tla)}}>Edit</a>
-            <a href='#' className='text-white  font-bold py-1 px-3 mx-1 rounded text-xs bg-red-600 hover:bg-red-800 hover:text-white' onClick={() => {showDeleteModal(); fetchTeam(tla)}}>Delete</a>
+            <Button text='View' color='blue' onClick={()=>{}}/>
+            <Button text='Edit' color='green' onClick={() => {showEditModal(); fetchTeam(tla)}}/>
+            <Button text='Delete' color='red' onClick={() => {showDeleteModal(); fetchTeam(tla)}}/>
           </td>
         </tr>
         </tbody>
